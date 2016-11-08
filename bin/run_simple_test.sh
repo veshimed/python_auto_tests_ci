15f7a1e461ca985444b8d42319cfaed3ee402835
Sergey <veshimed@users.noreply.github.com>
2016-11-08 10:33:11 +0200

    Edit path to test
    Needed to run this file from Jenkins

--- bin/run_simple_test.sh
+++ bin/run_simple_test.sh
@@ -3,4 +3,4 @@
 dateAndTime=$(date +"%Y%m%d-%H%M%S")
 xml_filename="../test_results/TEST-${dateAndTime}.xml"
 
-pytest -s -vv --junitxml=${xml_filename} ../tests/simple_test.py
+pytest -s -vv --junitxml=${xml_filename} tests/simple_test.py
